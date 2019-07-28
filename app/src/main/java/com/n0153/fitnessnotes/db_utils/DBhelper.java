86eb5afe8bf53331fcef0f0fa28d0f2a77553c29
armalyon <a.r.malyon@gmail.com>
2019-07-28 11:24:40 +0300

    dbhelper exerises created

--- app/src/main/java/com/n0153/fitnessnotes/db_utils/DBhelper.java
+++ app/src/main/java/com/n0153/fitnessnotes/db_utils/DBhelper.java
@@ -3,11 +3,28 @@ package com.n0153.fitnessnotes.db_utils;
 import android.content.Context;
 import android.database.sqlite.SQLiteDatabase;
 import android.database.sqlite.SQLiteOpenHelper;
+import android.util.Log;
 
 public class DBhelper extends SQLiteOpenHelper {
 
     public final static String DB_NAME = "Fit DB";
     public final static int DB_VER = 1;
+
+    public static final String KEY_ID = "_id";
+    public static final String KEY_NAME = "name";
+    public static final String KEY_CATEGORY = "category";
+    public static final String KEY_TYPE = "type";
+    public static final String KEY_UNITS = "units";
+    public static final String TYPE_TEXT_COMMA = " text, ";
+    public static final String TYPE_TEXT = " text ";
+    public static final String TYPE_INTEGER = " integer, ";
+
+    public static final String TABLE_EXERISES_NAME = " EXERSISES";
+
+
+
+    private final static String LOG_TAG = "DB Helper log";
+
 
     public DBhelper(Context context) {
         super(context, DB_NAME, null, DB_VER);
@@ -15,6 +32,12 @@ public class DBhelper extends SQLiteOpenHelper {
 
     @Override
     public void onCreate(SQLiteDatabase db) {
+        Log.d(LOG_TAG, "DB Creating");
+
+        db.execSQL("CREATE TABLE " + TABLE_EXERISES_NAME + " (" + KEY_ID + "integer primary key, " +
+                        KEY_CATEGORY + TYPE_TEXT_COMMA + KEY_NAME + TYPE_TEXT_COMMA +  KEY_TYPE +
+                            TYPE_INTEGER + KEY_UNITS + TYPE_TEXT + " )");
+
 
     }
 
