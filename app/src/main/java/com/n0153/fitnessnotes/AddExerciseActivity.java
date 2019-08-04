213bc97f044801ee68a33f6e55d94494ec62572e
armalyon <a.r.malyon@gmail.com>
2019-08-04 18:27:35 +0300

    table categories added + find add categ button

--- app/src/main/java/com/n0153/fitnessnotes/AddExerciseActivity.java
+++ app/src/main/java/com/n0153/fitnessnotes/AddExerciseActivity.java
@@ -5,15 +5,25 @@ import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.ImageButton;
 
-public class AddExerciseActivity extends AppCompatActivity {
+public class AddExerciseActivity extends AppCompatActivity implements View.OnClickListener {
 
+    ImageButton addCategoryBtn;
 
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_add_exercise);
+
+        addCategoryBtn = findViewById(R.id.addCategoryButton);
+        addCategoryBtn.setOnClickListener(this);
+
+
 
     }
 
@@ -31,5 +41,18 @@ public class AddExerciseActivity extends AppCompatActivity {
 
 
         return true;
+    }
+
+
+    @Override
+    public void onClick(View v) {
+
+        switch (v.getId()){
+            case (R.id.addCategoryButton):
+
+
+                break;
+        }
+
     }
 }
