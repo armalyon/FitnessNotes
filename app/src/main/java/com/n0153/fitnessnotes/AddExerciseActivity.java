c75518188b56bf0840bf57718625357de4d35005
armalyon <a.r.malyon@gmail.com>
2019-08-04 12:30:40 +0300

    add exercise avtivity accembled

--- app/src/main/java/com/n0153/fitnessnotes/AddExerciseActivity.java
+++ app/src/main/java/com/n0153/fitnessnotes/AddExerciseActivity.java
@@ -3,6 +3,7 @@ package com.n0153.fitnessnotes;
 import android.support.v7.app.ActionBar;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.Menu;
 
 public class AddExerciseActivity extends AppCompatActivity {
 
@@ -15,7 +16,14 @@ public class AddExerciseActivity extends AppCompatActivity {
         setContentView(R.layout.activity_add_exercise);
 
         actionBar = getSupportActionBar();
+        assert actionBar != null;
         actionBar.setTitle(R.string.title_add_ex);
 
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        getMenuInflater().inflate(R.menu.menu_save_ex, menu);
+        return true;
     }
 }
