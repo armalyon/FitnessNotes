968b31806c872c1ff6cb6b5822b013d1aa4f4a3f
Andriy Malyon <andriy.malyon@scantrust.com>
2019-09-06 17:38:36 +0300

    view workout fragment dialog added and can be shown on long click

---
+++ app/src/main/java/com/n0153/fitnessnotes/dialogs/ViewWorkoutFragment.java
@@ -0,0 +1,25 @@
+package com.n0153.fitnessnotes.dialogs;
+
+import android.os.Bundle;
+import android.support.v4.app.DialogFragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import com.n0153.fitnessnotes.R;
+
+public class ViewWorkoutFragment extends DialogFragment implements View.OnClickListener {
+
+
+    public View onCreateView( LayoutInflater inflater, ViewGroup container,  Bundle savedInstanceState) {
+        View v = inflater.inflate(R.layout.fragment_view_workout, container, false);
+
+
+        return v;
+    }
+
+    @Override
+    public void onClick(View v) {
+
+    }
+}
--- app/src/main/java/com/n0153/fitnessnotes/fragments/HistoryTabFragment.java
+++ app/src/main/java/com/n0153/fitnessnotes/fragments/HistoryTabFragment.java
@@ -8,26 +8,31 @@ import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
 import android.widget.ListView;
+import android.widget.Toast;
 
 import com.n0153.fitnessnotes.NewSetActivity;
 import com.n0153.fitnessnotes.R;
 import com.n0153.fitnessnotes.adapters.SetOptionsListAdapter;
 import com.n0153.fitnessnotes.db_utils.DBhelper;
 import com.n0153.fitnessnotes.db_utils.SetOptionsData;
+import com.n0153.fitnessnotes.dialogs.ViewWorkoutFragment;
 
 import java.util.ArrayList;
 import java.util.List;
 
 
-public class HistoryTabFragment extends Fragment {
+public class HistoryTabFragment extends Fragment implements AdapterView.OnItemLongClickListener {
 
     private DBhelper dBhelper;
     private String name;
     private String type;
     private ListView mainListView;
+    private ViewWorkoutFragment viewWorkoutDialog;
     private String LOG_TAG = "History tab";
+    private String dialogViewWorkoutTag = "start dialog view workout";
 
     @Override
     public void onCreate(Bundle savedInstanceState) {
@@ -37,7 +42,7 @@ public class HistoryTabFragment extends Fragment {
         dBhelper = new DBhelper(getContext());
         name = ((NewSetActivity) getActivity()).getLabel();
         type = dBhelper.getExeriseType(name);
-
+        viewWorkoutDialog = new ViewWorkoutFragment();
 
         Log.d(LOG_TAG, "onCreate Finished");
     }
@@ -50,6 +55,7 @@ public class HistoryTabFragment extends Fragment {
         // Inflate the layout for this fragment
         View v = inflater.inflate(R.layout.fragment_history_tab, container, false);
         mainListView = v.findViewById(R.id.historyList);
+        mainListView.setOnItemLongClickListener(this);
 
         updateMainlist();
         Log.d(LOG_TAG, "onCreateView finished");
@@ -66,8 +72,18 @@ public class HistoryTabFragment extends Fragment {
         SetOptionsListAdapter adapter = new SetOptionsListAdapter(getContext(), setOptionsList, type);
         mainListView.setAdapter(adapter);
 
+
     }
 
+
+    @Override
+    public boolean onItemLongClick(AdapterView<?> parent, View view, int position, long id) {
+
+        viewWorkoutDialog.show(getFragmentManager(), dialogViewWorkoutTag);
+
+
+        return false;
+    }
 }
 
 
