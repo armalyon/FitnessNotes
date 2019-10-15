9fbf1962773cefe2f1bb70daf667aea3255fce37
armalyon <a.r.malyon@gmail.com>
2019-10-15 13:15:04 +0300

    just for moving

---
+++ app/src/main/java/com/n0153/fitnessnotes/dialogs/RenameCategoryFragment.java
@@ -0,0 +1,31 @@
+package com.n0153.fitnessnotes.dialogs;
+
+
+import android.os.Bundle;
+import android.support.v4.app.DialogFragment;
+import android.support.v4.app.Fragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import com.n0153.fitnessnotes.R;
+
+/**
+ * A simple {@link Fragment} subclass.
+ */
+public class RenameCategoryFragment extends DialogFragment {
+
+
+    public RenameCategoryFragment() {
+        // Required empty public constructor
+    }
+
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        // Inflate the layout for this fragment
+        return inflater.inflate(R.layout.fragment_rename_category, container, false);
+    }
+
+}
